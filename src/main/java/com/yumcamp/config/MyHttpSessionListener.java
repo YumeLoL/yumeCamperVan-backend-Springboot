c5570dc733ff734d278613d8b0ebdc06dc8fc628
yume <yumeee.lol@gmail.com>
2023-04-06 18:00:39 +1000

    set a session expire time(60)

---
+++ src/main/java/com/yumcamp/config/MyHttpSessionListener.java
@@ -0,0 +1,13 @@
+package com.yumcamp.config;
+
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyHttpSessionListener implements HttpSessionListener {
+    @Override
+    public void sessionCreated(HttpSessionEvent event) {
+        event.getSession().setMaxInactiveInterval(60);
+    }
+}
